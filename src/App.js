8984c20321cc628ece272c44bf2636593df535e1
David <davidsusairaj1996@gmail.com>
2022-02-08 05:24:23 +0530

    REACT-REDUX-COUNTER-APP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, reset } from "./actions";
+// import { useSelector, useDispatch } from "react-redux";
+// const dispatch = useDispatch();
+// const counter = useSelector((state) => state.count);
+// let count = 0;
+// function counterReducer(state = count, action) {
+//   switch (action.type) {
+//     case "increment":
+//       return state + 1;
+//     case "decrement":
+//       return state.value - 1;
+//     default:
+//       return state;
+//   }
+// }
+
+// const store = createStore(counterReducer);
+// store.subscribe(() => console.log(store.getState()));
+
+// store.dispatch({ type: "increment" });
+
+//  const increment = () => {
+//   return {
+//     type: "INCREMENT",
+//   };
+// };
+
+//  const decrement = () => {
+//   return {
+//     type: "DECREMENT",
+//   };
+// };
+// const reset = () => {
+//   return {
+//     type: "RESET",
+//   };
+// };
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>COUNTER: {counter} </h1>
+      <button onClick={() => dispatch(increment())}>PLUS +1</button>
+      <button onClick={() => dispatch(decrement())}>MINUS -1</button>
+      <button onClick={() => dispatch(reset())}>RESET</button>
     </div>
   );
 }
---
+++ src/reducers/isLogged.js
@@ -0,0 +1,11 @@
+const isLogged = (state = false, action) => {
+  switch (action.type) {
+    case "SIGN_IN":
+      return !state;
+      break;
+
+    default:
+      return state;
+  }
+};
+export default isLogged;
